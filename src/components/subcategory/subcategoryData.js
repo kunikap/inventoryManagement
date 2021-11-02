152a30d5e39aa483eb2db452978e53300d38442e
kunika FE <kunika@doodleblue.com>
2021-11-02 18:27:31 +0530

    handled unlimited

--- src/components/subcategory/subcategoryData.js
+++ src/components/subcategory/subcategoryData.js
@@ -5,20 +5,46 @@ const Subcategory = ({ name, color, options, skuId, stocks, onSave }) => {
   const [editMode, setEditMode] = useState(false);
   const [isUnlimited, setIsUnlimited] = useState(false);
   const [stockValue, setStockValue] = useState(stocks);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  if (stocks === "Unlimited" && !isUnlimited)  {
+    setIsUnlimited(true);
+    setStockValue("");
+  }
+  
   const handleStockInput = (event) => {
     const value = event.target.value;
-    setStockValue(value);
+    if (checkForNumber(value)) {
+      setStockValue(value);
+      setErrorMessage("");
+    }
+    if (value === "" && !isUnlimited) {
+      setErrorMessage("Please enter stock value");
+    }
+    if (value === "" && isUnlimited) {
+      setStockValue("");
+      setErrorMessage("");
+    }
+  };
+
+  const checkForNumber = (value) => {
+    const numberRegex = /^[0-9\b]+$/;
+    return value === "" || numberRegex.test(value);
   };
 
   const handleUnlimitedChange = (event) => {
-    const value = event.target.value;
+    const value = event.target.checked;
     setIsUnlimited(value);
+    if (value) {
+      setErrorMessage("");
+    }
   };
 
-  const saveModifications = ()=> {
-    onSave(stockValue, skuId);
+  const saveModifications = () => {
+    const value = isUnlimited && stockValue === "" ? "Unlimited": stockValue;
+    onSave(value, skuId);
     setEditMode(!editMode);
-  }
+  };
   return (
     <tr>
       <td>{name}</td>
@@ -30,7 +56,7 @@ const Subcategory = ({ name, color, options, skuId, stocks, onSave }) => {
           {editMode ? (
             <div>
               <input
-                type="number"
+                type="text"
                 value={stockValue}
                 placeholder="Enter value"
                 onChange={handleStockInput}
@@ -46,6 +72,7 @@ const Subcategory = ({ name, color, options, skuId, stocks, onSave }) => {
           ) : (
             stocks
           )}
+          {errorMessage && <span>{errorMessage}</span>}
         </td>
       }
       {
