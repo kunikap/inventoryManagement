adb2200c1e56b2b19a346c6142687d118d5ed030
kunika FE <kunika@doodleblue.com>
2021-11-02 14:07:04 +0530

    push some change

--- src/components/subcategory/subcategoryData.js
+++ src/components/subcategory/subcategoryData.js
@@ -3,13 +3,46 @@ import "./subcategoryData.css";
 
 const Subcategory = ({ name, color, options, skuId, stocks, onSave }) => {
   const [editMode, setEditMode] = useState(false);
+  const [isUnlimited, setIsUnlimited] = useState(false);
+  const [stockValue, setStockValue] = useState(stocks);
+  const handleStockInput = (event) => {
+    const value = event.target.value;
+    setStockValue(value);
+  };
+
+  const handleUnlimitedChange = (event) => {
+    const value = event.target.value;
+    setIsUnlimited(value);
+  };
   return (
     <tr>
       <td>{name}</td>
       <td>{color}</td>
       <td>{options}</td>
       <td>{skuId}</td>
-      {<td>{stocks}</td>}
+      {
+        <td>
+          {editMode ? (
+            <div>
+              <input
+                type="number"
+                value={stockValue}
+                placeholder="Enter value"
+                onChange={handleStockInput}
+              />
+              <input
+                type="checkbox"
+                name="stocksValue"
+                value={isUnlimited}
+                onChange={handleUnlimitedChange}
+              />
+              <label for="stocksValue">Unlimited</label>
+            </div>
+          ) : (
+            stocks
+          )}
+        </td>
+      }
       {
         <td>
           {editMode ? (
