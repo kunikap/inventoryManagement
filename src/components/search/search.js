21d724c54efbd708617149aa21c578b6b0ffcf52
kunika FE <kunika@doodleblue.com>
2021-11-02 21:54:16 +0530

    Added search functionality

--- src/components/search/search.js
+++ src/components/search/search.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import "./search.css";
 
-const Search = () => {
+const Search = ({ onSearch }) => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const handleSearch = (event) => {
+    const value = event.target.value;
+    setSearchTerm(value);
+  };
   return (
     <div className="search-wrapper">
-        <div className="search-bar">
-          <i className="fa fa-search search-icon search-icon"></i>
-          <input type="text" className="search-input" placeholder="Search by Item Name, SKU id…"/>
-        </div>
-        <button className="search-button">Search</button>
+      <div className="search-bar">
+        <i className="fa fa-search search-icon search-icon"></i>
+        <input
+          type="text"
+          className="search-input"
+          placeholder="Search by Item Name, SKU id…"
+          onChange={handleSearch}
+          value={searchTerm}
+        />
       </div>
+      <button
+        className="search-button"
+        onClick={onSearch.bind(this, searchTerm)}
+      >
+        Search
+      </button>
+    </div>
   );
 };
 
